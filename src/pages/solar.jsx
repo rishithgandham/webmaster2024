f15382cb739884eda0dc58a824b6c05d86c1ce0f
Rishith Gandham <rishith.gandham@gmail.com>
2024-03-20 21:56:32 -0400

    added solar page

---
+++ src/pages/solar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Solar = () => {
+  return (
+    <>
+      <main className=" min-h-screen  h-full w-full bg-gradient-to-b from-black to-slate-950 text-white ">
+        <div className="flex flex-col w-full h-full lg:px-52 md:px-20 px-10 xl:px-72 py-28  ">
+            <div className="title text-3xl font-bold">Solar Energy For Homes</div>
+
+        </div>
+      </main>
+    </>
+  );
+};
+
+const texts = {
+    overview: {
+
+    }
+}
+
+export default Solar;
