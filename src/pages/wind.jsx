e9999a502cb39893b4e439dc9f7416e50cc17500
tanishqmittal <tanishqmittal139@gmail.com>
2024-03-23 16:24:05 -0400

    addded images to wind page

--- src/pages/wind.jsx
+++ src/pages/wind.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { motion } from "framer-motion";
 import logo from "./logo.png";
 import map from "./map.png";
+import wind_start from "./wind_two.jpg";
+import wind_side from "./wind_three.jpg";
+import wind_gif from "./windmill_03.gif";
+import wind_turbine from "./wind_turbine.jpg";
 import Slider from "react-slick";
 const Wind = () => {
   const variants = {
@@ -45,11 +49,23 @@ const Wind = () => {
               </div>
             </div>
           </div>
+          <div className="image w-full my-10 ">
+            <motion.img
+              variants={variants}
+              initial="hidden"
+              animate="visible"
+              height={1080}
+              width={1920}
+              src={wind_start}
+              alt="wind_start"
+              className="wind rounded-3xl aspect-video object-cover"
+            />
+          </div>
           <motion.div
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="text-3xl font-bold pt-20"
+            className="text-3xl font-extrabold"
           >
             Overview
           </motion.div>
@@ -83,7 +99,7 @@ const Wind = () => {
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="text-3xl font-bold pt-20"
+            className="text-3xl font-extrabold pt-20"
           >
             Advantages Of Wind Power In Renewable Energy Production
           </motion.div>
@@ -99,8 +115,8 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Abundant and Renewable</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">Abundant and Renewable</div>
+              <div className="text-lg pt-4 font-semibold">
                 Wind energy is obtained from the natural movement of air masses,
                 making it a virtually infinite supply. Wind energy will be
                 accessible to capture as long as the sun continues to heat the
@@ -113,8 +129,10 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Clean and Environmentally Friendly</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">
+                Clean and Environmentally Friendly
+              </div>
+              <div className="text-lg pt-4 font-semibold">
                 Wind power generating emits no greenhouse gases, air pollutants,
                 or water pollutants during operation. It dramatically decreases
                 dependency on fossil fuels, lowering air pollution, addressing
@@ -127,8 +145,8 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Cost-Effective</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">Cost-Effective</div>
+              <div className="text-lg pt-4 font-semibold">
                 Wind energy has become much more affordable in recent years
                 thanks to technological improvements, economies of scale, and
                 simplified manufacturing processes. Wind power is presently one
@@ -143,8 +161,10 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Energy Independence and Security</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">
+                Energy Independence and Security
+              </div>
+              <div className="text-lg pt-4 font-semibold">
                 Wind energy is a decentralized energy source that enables towns,
                 regions, and countries to diversify their energy portfolios and
                 lessen their reliance on imported fossil fuels. This improves
@@ -158,8 +178,10 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Job Creation and Economic Benefits</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">
+                Job Creation and Economic Benefits
+              </div>
+              <div className="text-lg pt-4 font-semibold">
                 The wind energy industry supports a wide range of jobs across
                 various sectors, including manufacturing, construction,
                 installation, operations, maintenance, and research. Wind power
@@ -174,8 +196,10 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Scalability and Flexibility</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">
+                Scalability and Flexibility
+              </div>
+              <div className="text-lg pt-4 font-semibold">
                 Wind power projects can range from small-scale installations for
                 individual homes or businesses to massive utility-scale wind
                 farms that cover extensive regions. This flexibility enables
@@ -185,15 +209,15 @@ const Wind = () => {
               </div>
             </motion.div>
             <motion.div
-              variants={variants}
+              ariants={variants}
               initial="hidden"
               animate="visible"
-              className="border border-gray-300 rounded-lg p-6"
+              className="border border-gray-300 rounded-lg p-6 flex flex-col"
             >
-              <div className="text-2xl">
+              <div className="text-2xl font-bold">
                 Technological Innovation and Growth Potential
               </div>
-              <div className="text-lg pt-4">
+              <div className="text-lg pt-4 font-semibold">
                 Continued research and development in wind energy technology
                 promises to enhance efficiency, dependability, and energy
                 output. Offshore wind, in particular, has enormous growth
@@ -202,12 +226,30 @@ const Wind = () => {
                 of large-scale wind farms with minimum land use effect.
               </div>
             </motion.div>
+            <div className="image w-full my-10 ">
+              <motion.img
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                height={1080}
+                width={1920}
+                src={wind_gif}
+                alt="wind_gif"
+                className="wind rounded-3xl aspect-sqaure h-full object-cover"
+              />
+              <motion.div ariants={variants} initial="hidden" animate="visible">
+                <div className="mb-4 flex justify-center text-xs font-bold mt-2">
+                  {" "}
+                  A single wind turbine can power 500 homes.{" "}
+                </div>
+              </motion.div>
+            </div>
           </motion.div>
           <motion.div
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="text-3xl font-bold pt-20"
+            className="text-3xl font-extrabold pt-20"
           >
             Information On Wind Turbines
           </motion.div>
@@ -215,7 +257,7 @@ const Wind = () => {
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="grid grid-cols-1 lg:grid-cols-2 gap-10 pt-10"
+            className="grid grid-cols-5 grid-rows-5 gap-4 pt-10"
           >
             <motion.div
               variants={variants}
@@ -223,8 +265,8 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Types of wind turbines</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">Types of wind turbines</div>
+              <div className="text-lg pt-4 font-semibold">
                 There are two primary types of wind turbines: horizontal axis
                 and vertical axis. Horizontal-axis turbines, which are
                 positioned parallel to the ground, are the most frequent and
@@ -241,10 +283,10 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">
+              <div className="text-2xl font-bold ">
                 Suitable locations for wind turbine installation
               </div>
-              <div className="text-lg pt-4">
+              <div className="text-lg pt-4 font-semibold">
                 Wind turbines thrive in places where wind speeds are steady and
                 powerful, averaging 6-9 meters per second (m/s). Coastal
                 regions, plains, ridges, and wide landscapes are appropriate for
@@ -258,8 +300,10 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Environmental considerations</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">
+                Environmental considerations
+              </div>
+              <div className="text-lg pt-4 font-semibold">
                 Proper siting, environmental impact assessments, and mitigation
                 strategies are critical for reducing negative effects on
                 species, habitats, and landscapes during wind turbine
@@ -267,19 +311,32 @@ const Wind = () => {
               </div>
             </motion.div>
           </motion.div>
+          <div className="image w-full my-10 ">
+            <motion.img
+              variants={variants}
+              initial="hidden"
+              animate="visible"
+              height="100%"
+              width="100%"
+              src={wind_side}
+              alt="wind_side"
+              className="wind rounded-3xl aspect-video object-cover"
+            />
+          </div>
           <motion.div
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="text-3xl font-bold pt-20"
+            className="text-3xl font-extrabold pt-20"
           >
             Cost
           </motion.div>
+
           <motion.div
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="grid grid-cols-1 lg:grid-cols-2 gap-10 pt-10"
+            className="grid grid-cols-2 grid-rows-3 gap-10 pt-10"
           >
             <motion.div
               variants={variants}
@@ -287,8 +344,10 @@ const Wind = () => {
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Breakdown of turbine costs</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">
+                Breakdown of turbine costs
+              </div>
+              <div className="text-lg pt-4 font-semibold">
                 Turbine prices vary depending on size, capacity, and technology,
                 with average utility-scale turbines costing between $2 million
                 and $4 million. Installation costs include site preparation,
@@ -301,10 +360,10 @@ const Wind = () => {
               variants={variants}
               initial="hidden"
               animate="visible"
-              className="border border-gray-300 rounded-lg p-6"
+              className="col-start-1 row-start-2 border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Installation expenses</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">Installation expenses</div>
+              <div className="text-lg pt-4 font-semibold">
                 Installation costs include site preparation, foundation
                 construction, transportation, crane hire, and labor.
                 Installation costs for onshore projects can vary from $40,000 to
@@ -316,10 +375,12 @@ const Wind = () => {
               variants={variants}
               initial="hidden"
               animate="visible"
-              className="border border-gray-300 rounded-lg p-6"
+              className="col-start-1 row-start-3 border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl">Long-term maintenance costs</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold">
+                Long-term maintenance costs
+              </div>
+              <div className="text-lg pt-4 font-semibold">
                 Long-term maintenance expenditures generally range between 1-2%
                 of the overall project cost every year. Inspections,
                 lubrication, blade cleaning, and component replacement are all
@@ -328,12 +389,22 @@ const Wind = () => {
                 turbine age and operating circumstances.
               </div>
             </motion.div>
+            <div className="row-span-3 col-start-2 row-start-1  image w-full h-full">
+              <motion.img
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                src={wind_turbine}
+                alt="wind_turbine"
+                className="wind rounded-3xl aspect-video object-cover h-full w-full"
+              />
+            </div>
           </motion.div>
           <motion.div
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="text-3xl font-bold pt-20"
+            className="text-3xl font-extrabold pt-20"
           >
             Tax Rebate Information
           </motion.div>
@@ -341,18 +412,13 @@ const Wind = () => {
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="grid grid-cols-1 lg:grid-cols-2 gap-10 pt-10"
+            className="pt-10"
           >
-            <motion.div
-              variants={variants}
-              initial="hidden"
-              animate="visible"
-              className="border border-gray-300 rounded-lg p-6"
-            >
-              <div className="text-2xl">
+            <motion.div variants={variants} initial="hidden" animate="visible">
+              <div className="text-2xl font-bold">
                 Available Tax Credits and Incentives for Wind Energy
               </div>
-              <div className="text-lg pt-4">
+              <div className="text-lg pt-4 font-semibold">
                 Federal tax credits, such as the PTC and ITC, provide financial
                 incentives for wind energy installations. State-level incentives
                 for wind energy development may include rebates, subsidies, and
@@ -368,24 +434,19 @@ const Wind = () => {
             variants={variants}
             initial="hidden"
             animate="visible"
-            className="text-3xl font-bold pt-20"
+            className="text-3xl font-extrabold pt-20 mb-10"
           >
             Best Practices for Wind Turbine Maintenance
           </motion.div>
-          <motion.div
-            variants={variants}
-            initial="hidden"
-            animate="visible"
-            className="grid grid-cols-1 lg:grid-cols-2 gap-10 pt-10"
-          >
+          <motion.div variants={variants} initial="hidden" animate="visible">
             <motion.div
               variants={variants}
               initial="hidden"
               animate="visible"
               className="border border-gray-300 rounded-lg p-6"
             >
-              <div className="text-2xl"> Maintenance Tips</div>
-              <div className="text-lg pt-4">
+              <div className="text-2xl font-bold"> Maintenance Tips</div>
+              <div className="text-lg pt-4 font-semibold">
                 Wind turbines require regular maintenance and inspection to
                 guarantee maximum performance, dependability, and safety. Best
                 practices include monitoring turbine functioning, lubricating
@@ -402,14 +463,9 @@ const Wind = () => {
             </motion.div>
           </motion.div>
         </div>
-        <motion.section
-          variants={variants}
-          initial="hidden"
-          animate="visible"
-          className="py-10"
-        >
+        <motion.section variants={variants} initial="hidden" animate="visible">
           <div className="flex flex-col items-center mb-8">
-            <div className="title text-4xl font-extrabold mt-4 mb-6">
+            <div className="title text-4xl font-extrabold mb-6">
               <motion.div
                 variants={variants}
                 initial="hidden"
